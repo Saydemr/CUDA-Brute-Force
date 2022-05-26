df25baa9214871d71a65b0a3f27573450a9d0508
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2022-05-26 16:55:42 +0300

    Hashcat script created

---
+++ checker.sh
@@ -0,0 +1,59 @@
+echo "Bash version ${BASH_VERSION}..."
+
+COMPLEXITY="d L Lc dLc"
+ENCODE="md5 sha1 sha256 sha512"
+
+for complexity in $COMPLEXITY
+do
+    if [[ $complexity -eq "d" ]]
+    then
+        charset="0123456789"
+    elif [[ $complexity -eq "L" ]]
+    then
+        charset="abcdghijklmnopqrstuvwxyz"
+    elif [[ $complexity -eq "Lc" ]]
+    then
+        charset="abcdghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    elif [[ $complexity -eq "dLc" ]]
+    then
+        charset="abcdghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    fi
+    
+    for encode in $ENCODE
+    do
+        if [[ $encode -eq "md5" ]]
+        then
+            type="0"
+        elif [[ $encode -eq "sha1" ]]
+        then
+            type="100"
+        elif [[ $encode -eq "sha256" ]]
+        then
+            type="1400"
+        elif [[ $encode -eq "sha512" ]]
+        then
+            type="1700"
+        fi
+
+        for length in {4..19}
+        do
+            input_file="./${complexity}/selected_${encode}_${length}.txt"
+            output_file="./${complexity}/selected_${encode}_${length}_out.txt"
+            echo ${length}
+            mask_str=""           
+            
+            for (( c=1; c<=${length}; c++ ))
+            do
+                mask_str+="?1"
+            done
+            echo ${mask_str}
+
+            while read -r line
+            do
+                hashcat -a 3 -m 0 ${line} -1 ${charset} ${mask_str} -O -w 3 >> ${output_file} 2>&1 &
+                wait
+            done < ${input_file}
+            
+        done
+    done
+done
